ef44cb6ee7ee788b067152a9f88d0084be449fc8
Edward Jimenez <edward.jimenez.m@gmail.com>
2017-04-21 16:11:05 -0500

    Test upload

---
+++ srcript.js
@@ -0,0 +1,18 @@
+var path = 'https://raw.github.com/Eddyjim/Plugbot/master/';
+
+console.log(path);
+
+getFirstSelectBox();
+
+function getFirstSelectBox(){
+	
+	/*ui-selectmenu-value*/
+	
+	var x = document.getElementsByClassName("ui-bizagi-render-control");
+	var i;
+	console.log("entered")
+	/*x[0].click();*/
+	for ( i = 0 ; i < x.length(); i++){
+		x[i].click();
+	}
+}
