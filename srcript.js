6fae9f40533c29344ff9e37994cb8ae9bfd0b71d
Edward Jimenez <edward.jimenez.m@gmail.com>
2017-04-24 17:44:40 -0500

    test

--- srcript.js
+++ srcript.js
@@ -4,12 +4,12 @@ console.log(path);
 
 getFirstSelectBox();
 
-$(".process").click(function(){
+$(function((".process").click(function(){
 		console.log("executed");
 		var x = document.getElementsByClassName("ui-selectmenu-value");
 		var i;
 		x[0].click();
-	});
+	}));
 
 function getFirstSelectBox(){
 	
