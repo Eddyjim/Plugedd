3c810d800ff8c44afb317dda2b539513120fa8d6
Edward Jimenez <edward.jimenez.m@gmail.com>
2017-04-25 08:46:22 -0500

    update

--- srcript.js
+++ srcript.js
@@ -2,21 +2,18 @@ var path = 'https://raw.github.com/Eddyjim/Plugbot/master/';
 
 console.log(path);
 
-getFirstSelectBox();
+//getFirstSelectBox();
 
-$(function((".process").click(function(){
-		console.log("executed");
-		var x = document.getElementsByClassName("ui-selectmenu-value");
-		var i;
-		x[0].click();
-	}));
+$(".process").click(function(){
+		console.log("executed append");
+		getFirstSelectBox();
+	});
 
 function getFirstSelectBox(){
 	
 	/*ui-selectmenu-value*/
-	
-	
-	
-	
+	var x = document.getElementsByClassName("ui-selectmenu-value");
+	var i;
+	x[0].click();
 	
 }
